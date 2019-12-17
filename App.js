f9139c1e41bff1b7b8035e8029a17176401cd2e7
Samuel Coleman <smcoleman19@gmail.com>
2019-12-17 12:41:47 -0700

    Add basic functionality for adding selected photo to state

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import * as Constants from 'expo-constants';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    image: null
+  }
+
+  render() {
+    let { image } = this.state
+    return (
+      <View style={styles.container}>
+        <Text onPress={() => this.selectImg()}>Open up App.js to start working on your app!</Text>
+        {image &&
+            <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      </View>
+    );
+  }
+
+selectImg() {
+  ImagePicker.requestCameraRollPermissionsAsync()
+    .then(response => {
+      if (response.granted === true) {
+        ImagePicker.launchImageLibraryAsync()
+          .then(response => this.setState({ image: response.uri }))
+      }
+    })
+  }
 }
 
 const styles = StyleSheet.create({
