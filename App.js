5cfdf038f8173623a22614a871de36427ac95edd
Sara <saralkarsh@gmail.com>
2019-12-17 19:10:35 -0700

    Render logo and app name on splash page

--- App.js
+++ App.js
@@ -3,17 +3,26 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import * as Constants from 'expo-constants';
+import * as Font from 'expo-font';
 
 export default class App extends React.Component {
   state = {
     image: null
   }
 
+  componentDidMount() {
+    Font.loadAsync({
+      'major-mono-display': require('./assets/fonts/MajorMonoDisplay-Regular.ttf'),
+    });
+  }
+
   render() {
     let { image } = this.state
     return (
       <View style={styles.container}>
-        <Text onPress={() => this.selectImg()}>Open up App.js to start working on your app!</Text>
+        <Image source={require('./assets/PupDatesLogo.png')} style={styles.image}/>
+        <Text style={styles.title}>PupDates</Text>
+        {/* <Text onPress={() => this.selectImg()}>Open up App.js to start working on your app!</Text> */}
         {image &&
             <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
       </View>
@@ -36,6 +45,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 70
+    // justifyContent: 'center',
   },
+  image: {
+    height: 140,
+    width: 140,
+  },
+  title: {
+    fontSize: 50,
+    fontFamily: 'major-mono-display'
+  }
 });
