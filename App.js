054b3ec6a2640f0d777c8151bdc6d10266b82334
Matthew Malone <m.malone@me.com>
2019-12-19 14:02:10 -0700

    Update the App file to add DogPack

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ import React, { Component } from 'react';
 const MainNavigator = createSwitchNavigator({
   Login: {screen: LoginScreen},
   Home: {screen: HomeScreen},
-  Menu: {screen: MenuScreen}
+  Menu: {screen: MenuScreen},
+  DogPack: {screen: DogPackScreen}
 })
 
 const AppContainer = createAppContainer(MainNavigator)
---
+++ components/DogPack/DogPack.js
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from '../Home/Home'
+import { Ionicons } from '@expo/vector-icons';
+
+class DogPackScreen extends React.Component {
+    static navigationOptions = ({navigation}) => ({        
+        headerLeft: () => <Image source={require('../../assets/PupDatesLogo.png')} style={styles.logo} />,
+        headerTitle: () => <Text style={styles.title}>PupDates</Text>,
+        headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                <Ionicons name="ios-close" size={32} color="green" />
+            </TouchableOpacity>
+        ),
+        headerStyle: {
+          backgroundColor: '#fff',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      })
+
+    render() {
+        return (
+            <SafeAreaView>
+                <View>
+                    <Text>This is the Menu!</Text>
+                    <Image source={require('../../assets/PupDatesLogo.png')} style={styles.image} onPress={() => console.log('wooooof')} />
+                    <Button onPress={() => this.props.navigation.navigate('Home')} title="Go Home" />
+                </View>
+            </SafeAreaView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      marginTop: 90
+    },
+    logo: {
+        height: 40,
+        width: 40,
+    }, 
+    image: {
+      height: 140,
+      width: 140,
+    },
+    title: {
+      fontSize: 50,
+      // fontFamily: 'major-mono-display'
+    },
+    input: {
+      height: 30,
+      width: '70%',
+      borderColor: 'lightgrey',
+      borderRadius: 50,
+      // borderColor: 'rgba(33,33,33,0.81)',
+      borderWidth: 1.5,
+      padding: 10,
+      height: 60,
+      margin: 20,
+      alignItems: 'center'
+    },
+    linearGradient: {
+      width: '70%',
+      borderRadius: 25,
+      height: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    button: {
+      // borderRadius: 50,
+      borderTopRightRadius: 20,
+      borderBottomRightRadius: 20,
+      borderBottomLeftRadius: 20,
+      borderTopLeftRadius: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'black'
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 25,
+    }
+  });
+const AppNavigator = createStackNavigator({
+    DogPackScreen: {
+        screen: DogPackScreen,
+    },
+});
+  export default createAppContainer(AppNavigator)
