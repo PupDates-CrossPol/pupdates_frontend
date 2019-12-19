10a5e05c3911a7f0e58633fb3979e561a2679329
Matthew Malone <m.malone@me.com>
2019-12-19 14:12:47 -0700

    All links to the new app components(pages) and import them into App.js

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import {createStackNavigator} from 'react-navigation-stack';
 import HomeScreen from './components/Home/Home'
 import LoginScreen from './components/LoginForm/LoginForm'
 import MenuScreen from './components/Menu/Menu'
+import DogPackScreen from './components/DogPack/DogPack'
+import MatchesScreen from './components/Matches/Matches'
+import UserProfileScreen from './components/UserProfile/UserProfile'
 import React, { Component } from 'react';
 
 const MainNavigator = createSwitchNavigator({
