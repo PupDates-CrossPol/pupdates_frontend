92c00eb2f873458aa5de2296796ee31f6fbe3974
Matthew Malone <m.malone@me.com>
2019-12-19 12:57:28 -0700

    Update App.js file to export AppContainer with createSwitchNavigator
    Co-authored-by: Sara Karsh <saralkarsh@gmail.com>

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import {createAppContainer} from 'react-navigation';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import HomeScreen from './components/Home/Home'
 import LoginScreen from './components/LoginForm/LoginForm'
+import React, { Component } from 'react';
 
-const MainNavigator = createStackNavigator({
+const MainNavigator = createSwitchNavigator({
   Login: {screen: LoginScreen},
   Home: {screen: HomeScreen},
 })
 
-const App = createAppContainer(MainNavigator)
+const AppContainer = createAppContainer(MainNavigator)
+
+class App extends Component {
+  render() {
+    return <AppContainer />
+  }
+}
 
 export default App
