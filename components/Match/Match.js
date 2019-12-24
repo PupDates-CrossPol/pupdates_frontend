bbdb22cc5cb25af6595f6f298dea56a6b08926ea
Matthew Malone <m.malone@me.com>
2019-12-23 21:30:24 -0800

    Create a dynamically adjustable matches component with dynamically adjusting pictures

--- components/Match/Match.js
+++ components/Match/Match.js
@@ -1,32 +1,114 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 
-const Match = () => {
-    return (
-        <View>
-            <View>
-                <TouchableOpacity>
-                    {/* icon goes here */}
-                <Text style={styles.componentTitle} >Menu</Text>
+const Match = ({matches}) => {
+    const dogPackImages = dogs => dogs.map( (dog, i) =>  {
+    const dogPlacement = `dogCircle${(i+1)}`
+    return <Image key={i} source={{uri: dog}} style={styles[dogPlacement]} />} );
+
+    const allMatches = matches.map( (match, i) => {
+        const {userImage, pack, userName} = match
+        return (
+            <View key={i} style={styles.matchViewIndividual}>
+                <TouchableOpacity style={styles.matchTouchableOpacity} onPress={() => console.log('TAKE ME TO THIS MATCH')} >
+                    <View style={styles.matchesImages}>
+                        <Image source={{uri: userImage}} style={styles.userCircle} />
+                        {dogPackImages(pack)}
+                    </View>
+                    <Text style={styles.matchUserNameText}>{userName}'s Pack</Text>
                 </TouchableOpacity>
-                <Text style={styles.componentTitle} >Matches</Text>
-                
-            </View>
-        </View>
+             </View>
+        );
+    });
+
+    return (
+       allMatches
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      marginTop: '2%',
-      marginBottom: '2%'
+    matchViewIndividual: {
+        borderBottomColor: 'rgba(0,0,0,0.57)',
+        borderBottomWidth: 1,
+        flexDirection: 'column',
+        width: '75%',
     },
-});
+    matchTouchableOpacity: {
+        padding: 0,
+        margin: 0,
+        paddingTop: 10,
+        marginLeft: '2%',
+    },
+    matchesImages: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        position: 'relative',
+    },
+    userCircle: {
+        aspectRatio: 1/1,
+        height: '75%',
+        borderRadius: 100,
+        borderColor: 'black',
+        borderWidth: 2,
+    },
+    dogCircle1: {
+        aspectRatio: 1/1,
+        height: '75%',
+        borderRadius: 100,
+        borderColor: 'black',
+        borderWidth: 2,
+        position: 'absolute',
+        left: '15%',
+    },
+    dogCircle2: {
+        aspectRatio: 1/1,
+        height: '75%',
+        borderRadius: 100,
+        borderColor: 'black',
+        borderWidth: 2,
+        position: 'absolute',
+        left: '30%',
+    },
+    dogCircle3: {
+        aspectRatio: 1/1,
+        height: '75%',
+        borderRadius: 100,
+        borderColor: 'black',
+        borderWidth: 2,
+        position: 'absolute',
+        left: '45%',
+    },
+    dogCircle4: {
+        aspectRatio: 1/1,
+        height: '75%',
+        borderRadius: 100,
+        borderColor: 'black',
+        borderWidth: 2,
+        position: 'absolute',
+        left: '60%',
+    },
+    dogCircle5: {
+        aspectRatio: 1/1,
+        height: '75%',
+        borderRadius: 100,
+        borderColor: 'black',
+        borderWidth: 2,
+        position: 'absolute',
+        left: '75%',
+    },
+    dogCircle6: {
+        aspectRatio: 1/1,
+        height: '75%',
+        borderRadius: 100,
+        borderColor: 'black',
+        borderWidth: 2,
+        position: 'absolute',
+        left: '90%',
+    },
+    matchUserNameText: {
+        fontSize: 25,
+        fontWeight: '300',
+    },
+})
 
 export default Match
