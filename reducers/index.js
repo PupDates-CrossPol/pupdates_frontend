d72d891efe26133fb8db9bf8f0ca93d3e3202b02
Matthew Malone <m.malone@me.com>
2020-01-03 22:39:23 -0700

    Update combineReducers to have packPhotos

--- reducers/index.js
+++ reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { user } from './user'
 import { pack } from './pack'
+import { packPhotos } from './packPhotos'
 
 export const rootReducer = combineReducers({
   user,
-  pack
+  pack,
+  packPhotos
 })
