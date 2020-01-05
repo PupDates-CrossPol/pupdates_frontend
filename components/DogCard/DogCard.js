d335b81a05edc10c8f62135ade4f105c4d874515
Matthew Malone <m.malone@me.com>
2020-01-04 21:34:22 -0700

    Setup DogInfo component

---
+++ components/DogCard/DogCard.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+
+const DogInfo = ({dogPack}) => {
+
+    return (
+        <View> 
+            <Text>THIS IS WHERE THE DOGS GO!!</Text>
+        </View>
+        )
+
+}
+
+export default DogInfo
