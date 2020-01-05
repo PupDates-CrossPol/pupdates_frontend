edd78c845cd6b43c57fa11c216535c294ca58154
Matthew Malone <m.malone@me.com>
2020-01-05 11:05:02 -0700

    Style add photo button on DogCard

--- components/DogCard/DogCard.js
+++ components/DogCard/DogCard.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity} from 'react-native';
 import { Col, Row, Grid } from "react-native-easy-grid";
+import { Ionicons } from '@expo/vector-icons';
 import { connect } from 'react-redux'
 import { setPackInfo, setPackPhotos } from '../../actions'
 
@@ -13,37 +14,39 @@ const DogCard = (props) => {
         
         return (
         <View key={i} style={styles.container}>
-            <Text>Name: {dog.name}</Text>
+            <Text>{dog.name}</Text>
             <Grid style={styles.grid}>
                 <Col>
-                    <Row>
+                    <Row style={styles.row}>
                         <Image key={i} source={{uri: 'https://images.pexels.com/photos/752383/pexels-photo-752383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}} style={styles.userCircle}/>
                     </Row>
-                    <Row>
-                        <Image key={i} source={{uri: 'https://images.pexels.com/photos/752383/pexels-photo-752383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}} style={styles.userCircle}/>
-                    </Row>
-                </Col>
-                <Col>
-                    <Row>
-                        <Image key={i} source={{uri: 'https://images.pexels.com/photos/752383/pexels-photo-752383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}} style={styles.userCircle}/>
-                    </Row>
-                    <Row>
+                    <Row style={styles.row}>
                         <Image key={i} source={{uri: 'https://images.pexels.com/photos/752383/pexels-photo-752383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}} style={styles.userCircle}/>
                     </Row>
                 </Col>
                 <Col>
-                    <Row>
+                    <Row style={styles.row}>
                         <Image key={i} source={{uri: 'https://images.pexels.com/photos/752383/pexels-photo-752383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}} style={styles.userCircle}/>
                     </Row>
-                    <Row>
+                    <Row style={styles.row}>
                         <Image key={i} source={{uri: 'https://images.pexels.com/photos/752383/pexels-photo-752383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}} style={styles.userCircle}/>
+                    </Row>
+                </Col>
+                <Col>
+                    <Row style={styles.row}>
+                        <Image key={i} source={{uri: 'https://images.pexels.com/photos/752383/pexels-photo-752383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}} style={styles.userCircle}/>
+                    </Row>
+                    <Row style={styles.row}>
+                        <TouchableOpacity style={styles.addPhoto}>
+                        <Ionicons name="ios-add" size={50} color='rgb(21, 112, 125)' />
+                        </TouchableOpacity>
                     </Row>
                 </Col>
             </Grid>
             <View style={styles.imageContainer}>
 
             </View>
-            {dogImages}
+            {/* {dogImages} */}
             <Text>Sex: {dog.sex}</Text>
             <Text>Breed: {dog.breed}</Text>
             <Text>Size: {dog.size}</Text>
@@ -66,6 +69,7 @@ const DogCard = (props) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        width: '100%'
       },
     menuCircle: {
       aspectRatio: 1/1,
@@ -77,13 +81,35 @@ const styles = StyleSheet.create({
     },
     grid: {
         width: '90%',
+        height: '50%',
+        borderRadius: 40,
+        borderColor: 'rgba(0,0,0,0.2)',
+        borderWidth: 2,
+    },
+    row: {
+        justifyContent: "center",
+        margin: '10%'
     },
     userCircle: {
         aspectRatio: 1/1,
-        height: '75%',
+        width: '90%',
         borderRadius: 100,
-        borderColor: 'black',
+        borderColor: 'rgba(0,0,0,0.2)',
         borderWidth: 2,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,  
+        elevation: 5
+    },
+    addPhoto: {
+        aspectRatio: 1/1,
+        width: '90%',
+        borderRadius: 100,
+        borderColor: 'rgba(0,0,0,0.2)',
+        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
   });
 
