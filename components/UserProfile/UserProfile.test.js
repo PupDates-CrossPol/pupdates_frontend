d89c8ab09447fc8ecc8900dee63ef2e3ab02b87b
Sara <saralkarsh@gmail.com>
2020-01-07 21:08:32 -0700

    Add tests for mapStateToProps and mapDispatchToProps

--- components/UserProfile/UserProfile.test.js
+++ components/UserProfile/UserProfile.test.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import * as actions from '../../actions/index'
 
-import { UserProfileScreen as UserProfile } from './UserProfile'
+import { UserProfileScreen as UserProfile, mapStateToProps, mapDispatchToProps } from './UserProfile'
+// import { mapStateToProps } from './UserProfile'
 
 describe('<UserProfile />', () => {
   it('has one child', () => {
@@ -24,5 +26,124 @@ describe('<UserProfile />', () => {
     }
     const tree = renderer.create(<UserProfile user={mockUser} />).toJSON()
     expect(tree).toMatchSnapshot()
+  })
+})
+
+describe('mapStateToProps', () => {
+  it('should return an object with the user object', () => {
+    const mockState = {
+      user: {
+        id: 1,
+        first_name: 'sara',
+        last_name: 'karsh',
+        email: 'email',
+        description: 'a string of words'
+      },
+      pack: [
+        {
+          id: 1,
+          user_id: 1,
+          name: 'fluffy',
+        }
+      ],
+      packPhotos: [
+        {
+          id: 1,
+          dog_id: 1,
+          image_url: 'someaddress'
+        }
+      ],
+      imageUpload: undefined,
+      tempUserImage: undefined
+    }
+
+    const expectedState = {
+      user: {
+        id: 1,
+        first_name: 'sara',
+        last_name: 'karsh',
+        email: 'email',
+        description: 'a string of words'
+      },
+      pack: [
+        {
+          id: 1,
+          user_id: 1,
+          name: 'fluffy',
+        }
+      ],
+      packPhotos: [
+        {
+          id: 1,
+          dog_id: 1,
+          image_url: 'someaddress'
+        }
+      ],
+      imageUpload: undefined,
+      tempUserImage: undefined
+    }
+
+    const mappedProps = mapStateToProps(mockState)
+
+    expect(mappedProps).toEqual(expectedState)
+  })
+})
+
+describe('mapDispatchToProps', () => {
+  it('setUserInfo should be dispatched with the correct action', () => {
+    const mockDispatch = jest.fn()
+
+    const mockUser = {
+      id: 2,
+      first_name: 'bob',
+      last_name: 'ross',
+      email: 'bobrossemail',
+      description: 'happy little trees'
+    }
+
+    const actionToDispatch = actions.setUserInfo(mockUser)
+    const mappedDispatch = mapDispatchToProps(mockDispatch)
+    mappedDispatch.setUserInfo(mockUser)
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+  })
+  it('setPackInfo should be dispatched with the correct action', () => {
+    const mockDispatch = jest.fn()
+    const mockPack = [
+      {
+        id: 1,
+        user_id: 1,
+        name: 'charles',
+        breed: 'lab',
+        size: 'large'
+      }
+    ]
+    const actionToDispatch = actions.setPackInfo(mockPack)
+    const mappedDispatch = mapDispatchToProps(mockDispatch)
+    mappedDispatch.setPackInfo(mockPack)
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+  })
+  it('setPackPhotos should be dispatched with the correct action', () => {
+    const mockDispatch = jest.fn()
+    const mockPackPhotos = [
+      {
+        id: 1,
+        dog_id: 1,
+        image_url: 'someaddress'
+      }
+    ]
+    const actionToDispatch = actions.setPackPhotos(mockPackPhotos)
+    const mappedDispatch = mapDispatchToProps(mockDispatch)
+    mappedDispatch.setPackPhotos(mockPackPhotos)
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+  })
+  it('setTempUserImage should be dispatched with the correct action', () => {
+    const mockDispatch = jest.fn()
+    const mockTempImage = {
+      uri: 'someurl'
+    }
+    const actionToDispatch = actions.setTempUserImage(mockTempImage)
+    const mappedDispatch = mapDispatchToProps(mockDispatch)
+    mappedDispatch.setTempUserImage(mockTempImage)
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
   })
 })
