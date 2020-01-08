2470172c4d3221f34df284854228594c90c65d76
Sara <saralkarsh@gmail.com>
2020-01-07 21:36:04 -0700

    Add mapstatetoprops test

--- components/SwipeDogCard/SwipeDogCard.test.js
+++ components/SwipeDogCard/SwipeDogCard.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { SwipeDogCard } from './SwipeDogCard'
+import { SwipeDogCard, mapStateToProps } from './SwipeDogCard'
 import { Provider } from 'react-redux'
 
 describe('<SwipeDogCard />', () => {
@@ -40,5 +40,54 @@ describe('<SwipeDogCard />', () => {
     const tree = renderer.create(<SwipeDogCard swipePack={mockSwipePack} swipePackPhotos={mockSwipePackPhotos} />).toJSON()
 
     expect(tree).toMatchSnapshot()
+  })
+})
+
+describe('mapStateToProps', () => {
+  it('should return an object with the swipePack and swipePackPhotos', () => {
+    const mockState = {
+      user: {
+        id: 1,
+        first_name: 'sara',
+        last_name: 'karsh',
+        email: 'email',
+        description: 'a string of words'
+      },
+      swipePack: [
+        {
+          id: 1,
+          user_id: 1,
+          name: 'fluffy',
+        }
+      ],
+      swipePackPhotos: [
+        {
+          id: 1,
+          dog_id: 1,
+          image_url: 'someaddress'
+        }
+      ]
+    }
+    
+    const expectedState = {
+      swipePack: [
+        {
+          id: 1,
+          user_id: 1,
+          name: 'fluffy',
+        }
+      ],
+      swipePackPhotos: [
+        {
+          id: 1,
+          dog_id: 1,
+          image_url: 'someaddress'
+        }
+      ]
+    }
+
+    const mappedProps = mapStateToProps(mockState)
+
+    expect(mappedProps).toEqual(expectedState)
   })
 })
